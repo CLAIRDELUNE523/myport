98bcaf744311e2afcbfc73c47abbab160065d041
clair <garreliazz2546newpc@gmail.com>
2023-10-25 12:24:14 +0900

    script

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function() {
+   var nav = document.querySelector('nav');
+   var header = document.querySelector('header');
+   if (window.scrollY > 400) { // スクロール位置の閾値を設定
+      header.classList.add('scrolled');
+   } else {
+      header.classList.remove('scrolled');
+   }
+   if(window.scrollY > 400){
+      nav.style.padding = '10px 20px'
+   }else{
+      nav.style.padding = '100px 30px'
+   }
+});
